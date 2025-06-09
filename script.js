f19d16b66adc24d4c666173a3f37a5b656763577
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-09 13:44:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function createTable() {
-    //Write your code here
-  
-}
+ function createTable()
+            {
+                let n=prompt("Enter rows");
+                let m=prompt("Enter columns");
+                
+                let str="";
+                 for(let i=0;i<n;i++)
+                {
+                    let tr=document.createElement("tr");
+                    for(let j=0;j<m;j++)
+                    {
+                       let td= document.createElement("td")
+                       td.innerText=`Row-${i} Column-${j}`
+                       tr.appendChild(td);
+
+                    }
+                    document.getElementById("myTable").appendChild(tr);
+                }
+
+            }
